beb4da778a25e8d4946de0b6ec2ae6c9cac74e04
yasinbyzli <yasinbeeyazli@gmail.com>
2021-05-04 21:02:09 +0300

    Clean Blog Projesi 2. Bolum
    public klasoru ve middleware olusturuldu, ejs paketi yuklendi, view ve partial klasörü olusturuldu, linkler ve yönlendirmeler yapıldı

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 const express = require('express')
-
+const ejs = require('ejs')
 const app = express()
 
+
+// TEMPLATE ENGİNE
+app.set('view engine', 'ejs')
+
+// MIDDLEWARE's
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-    const photo = {
-        id: 1,
-        title: "Blog title",
-        description: "Blog description"
-    }
-    res.send(photo)
+    res.render('index')
+})
+
+app.get('/about', (req, res) => {
+    res.render('about')
+})
+
+app.get('/add-post', (req, res) => {
+    res.render('add_post')
+})
+
+app.get('/post', (req, res) => {
+    res.render('post')
 })
 
 const port = 5000;
