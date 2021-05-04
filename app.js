2682c8b0133083b5bce796b4086c0b1a2624a1a7
yasinbyzli <yasinbeeyazli@gmail.com>
2021-05-04 15:20:53 +0300

    Workspace Started
    Proje paket dosyası oluşturuldu. Express ve nodemon paketleri yüklendi.

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    const photo = {
+        id: 1,
+        title: "Blog title",
+        description: "Blog description"
+    }
+    res.send(photo)
+})
+
+const port = 5000;
+
+app.listen(port, () => {
+    console.log(`Sunucu http://localhost:${port}/ adresinde başlatıldı`)
+})
